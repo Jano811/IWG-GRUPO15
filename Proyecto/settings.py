aa55ce1a1802f11ebce33bb6ca530f0f8654aafe
Kcarazas <145164617+Kcarazas@users.noreply.github.com>
2023-10-24 00:09:20 -0300

    mejoras en el fronted e implementacion del css

--- Proyecto/settings.py
+++ Proyecto/settings.py
@@ -1,3 +1,4 @@
+import os
 """
 Django settings for Proyecto project.
 
@@ -118,7 +119,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'Aplicacion1/static'),]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
